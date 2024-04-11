e70324e0d919630736e17b26a62247452c0ca2bd
Sajana <svwijesinghe97@gmail.com>
2024-04-11 00:06:19 -0500

    Fixed Bugs

--- script.js
+++ script.js
@@ -1,138 +1,177 @@
+// Setup canvas
 const canvas = document.getElementById("canvas");
-const contextVar = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // Canvas settings
-contextVar.fillStyle = 'white';
-contextVar.strokeStyle = 'white';
-contextVar.lineWidth = 1;
+ctx.fillStyle = "white";
+ctx.strokeStyle = "white";
+ctx.lineWidth = 1;
 
 class Particle {
-    constructor(effect) {
-        this.effect = effect;
-        this.x = Math.floor(Math.random() * this.effect.width);
-        this.y = Math.floor(Math.random() * this.effect.height);
-        this.speedX;
-        this.speedY;
-        this.speedModifier = Math.floor(Math.random() * 5 + 1);
-        this.history = [{x: this.x, y: this.y}];
-        this.maxLength = Math.floor(Math.random() * 240 + 10);
-        this.angle = 0;
-        this.timer = this.maxLength * 2;
-        this.colors = ['#4c026b', '#730d9e', '#9622c7', '#b44ae0', '#cd72f2']
-        this.color = this.colors[Math.floor(Math.random() * this.colors.length)];
+  constructor(effect) {
+    this.effect = effect;
+    this.x = Math.floor(Math.random() * this.effect.width);
+    this.y = Math.floor(Math.random() * this.effect.height);
+    this.speedX;
+    this.speedY;
+    this.speedModifier = Math.floor(Math.random() * 5 + 1);
+    this.history = [{ x: this.x, y: this.y }];
+    this.maxLength = Math.floor(Math.random() * 200 + 10);
+    this.angle = 0;
+    this.timer = this.maxLength * 2;
+    this.colors = [
+      "#3e0d4f",
+      "#39166e",
+      "#25259c",
+      "#335cb5",
+      "#4293c2",
+      "#5acedb",
+    ];
+    this.color = this.colors[Math.floor(Math.random() * this.colors.length)];
+  }
+
+  draw(context) {
+    context.beginPath();
+    context.moveTo(this.history[0].x, this.history[0].y);
+
+    for (let i = 0; i < this.history.length; i++) {
+      context.lineTo(this.history[i].x, this.history[i].y);
     }
+    context.strokeStyle = this.color;
+    context.stroke();
+  }
 
-    draw(context) {
-        context.beginPath();
-        context.moveTo(this.history[0].x, this.history[0].y);
+  update() {
+    this.timer--;
 
-        for (let i = 0; i < this.history.length; i++) {
-            context.lineTo(this.history[i].x, this.history[i].y);
-        }
-        context.strokeStyle = this.color;
-        context.stroke();
+    if (this.timer >= 1) {
+      let x = Math.floor(this.x / this.effect.cellSize);
+      let y = Math.floor(this.y / this.effect.cellSize);
+      let index = y * this.effect.cols + x;
+      this.angle = this.effect.flowField[index];
+
+      this.speedX = Math.cos(this.angle);
+      this.speedY = Math.sin(this.angle);
+      this.x += this.speedX * this.speedModifier;
+      this.y += this.speedY * this.speedModifier;
+
+      this.history.push({ x: this.x, y: this.y });
+
+      if (this.history.length > this.maxLength) {
+        this.history.shift();
+      }
+    } else if (this.history.length > 1) {
+      this.history.shift();
+    } else {
+      this.reset();
     }
+  }
 
-    update() {
-        this.timer--;
-
-        if (this.timer >= 1) {
-            let x = Math.floor(this.x / this.effect.cellSize);
-            let y = Math.floor(this.y / this.effect.cellSize);
-            let index = y * this.effect.cols + x;
-            this.angle = this.effect.flowField[index];
-    
-            this.speedX = Math.cos(this.angle);
-            this.speedY = Math.sin(this.angle);
-            this.x += this.speedX * this.speedModifier;
-            this.y += this.speedY * this.speedModifier;
-    
-            // Adds one element to the end of an array
-            this.history.push({x: this.x, y: this.y});
-    
-            if (this.history.length > this.maxLength) {
-                // Removes one element from the beginning of an array
-                this.history.shift();
-            }
-        } else if (this.history.length > 1) {
-            this.history.shift();
-        } else {
-            this.rest();
-        }
-    }
-
-    rest() {
-        this.x = Math.floor(Math.random() * this.effect.width);
-        this.y = Math.floor(Math.random() * this.effect.height);
-        this.history = [{x: this.x, y: this.y}];
-        this.timer = this.maxLength * 2;
-    }
+  reset() {
+    this.x = Math.floor(Math.random() * this.effect.width);
+    this.y = Math.floor(Math.random() * this.effect.height);
+    this.history = [{ x: this.x, y: this.y }];
+    this.timer = this.maxLength * 2;
+  }
 }
 
 class Effect {
-    constructor(canvas) {
-        this.canvas = canvas;
-        this.width = this.canvas.width;
-        this.height = this.canvas.height;
-        this.particles = [];
-        this.numberOfParticles = 1000;
-        this.cellSize = 5;
-        this.rows;
-        this.cols;
-        this.flowField = [];
-        this.curve = 2;
-        this.zoom = 0.05;
-        this.init();
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.particles = [];
+    this.numberOfParticles = 2000;
+    this.cellSize = 20;
+    this.rows;
+    this.cols;
+    this.flowField = [];
+    this.curve = 7;
+    this.zoom = 0.09;
+    this.debug = false;
+    this.init();
 
-        window.addEventListener('resize', e => {
-            this.resize(e.target.innerWidth, e.target.innerHeight);
-        })
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "d") {
+        this.debug = !this.debug;
+      }
+    });
+
+    window.addEventListener("resize", (e) => {
+      this.resize(e.target.innerWidth, e.target.innerHeight);
+    });
+  }
+
+  init() {
+    // Create flow field
+    this.rows = Math.floor(this.height / this.cellSize);
+    this.cols = Math.floor(this.width / this.cellSize);
+    this.flowField = [];
+
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        let angle =
+          Math.cos(x * this.zoom) + Math.sin(y * this.zoom) + this.curve;
+        this.flowField.push(angle);
+      }
     }
 
-    init() {
-        // Create flow field
-        this.rows = Math.floor(this.height / this.cellSize);
-        this.cols = Math.floor(this.width / this.cellSize);
-        this.flowField = [];
+    // Create particles
+    this.particles = [];
+    for (let i = 0; i < this.numberOfParticles; i++) {
+      this.particles.push(new Particle(this));
+    }
+  }
 
-        for (let y = 0; y < this.rows; y++) {
-            for (let x = 0; x < this.cols; x++) {
-                let angle = Math.cos(x * this.zoom) + Math.sin(y * this.zoom) + this.curve;
-                this.flowField.push(angle);
-            }
-        }
-
-        // Create particles
-        this.particles = [];
-        for (let i = 0; i < this.numberOfParticles; i++) {
-            this.particles.push(new Particle(this));
-        }
+  drawGrid(context) {
+    context.save();
+    context.strokeStyle = "red";
+    context.lineWidth = 0.3;
+    for (let c = 0; c < this.cols; c++) {
+      context.beginPath();
+      context.moveTo(this.cellSize * c, 0);
+      context.lineTo(this.cellSize * c, this.height);
+      context.stroke();
     }
 
-    resize(width, height) {
-        this.canvas.width = width;
-        this.canvas.height = height;
-        this.width = this.canvas.width;
-        this.height = this.canvas.height;
-        this.init();
+    for (let r = 0; r < this.rows; r++) {
+      context.beginPath();
+      context.moveTo(0, this.cellSize * r);
+      context.lineTo(this.width, this.cellSize * r);
+      context.stroke();
+    }
+    context.restore();
+  }
+
+  resize(width, height) {
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.init();
+  }
+
+  render(context) {
+    if (this.debug) {
+      this.drawGrid(context);
     }
 
-    render(context) {
-        this.particles.forEach(particle => {
-            particle.draw(context);
-            particle.update();
-        })
-    }
-}
-
-// Create the illusion of motion
-function animate() {
-    contextVar.clearRect(0, 0, canvas.width, canvas.height)
-    effect.render(contextVar);
-    requestAnimationFrame(animate);
+    this.particles.forEach((particle) => {
+      particle.draw(context);
+      particle.update();
+    });
+  }
 }
 
 const effect = new Effect(canvas);
-animate()
+console.log(effect);
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  effect.render(ctx);
+  requestAnimationFrame(animate);
+}
+
+animate();
